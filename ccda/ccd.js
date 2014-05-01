d617bcadecd56efa60e785e8c954b13cc2d770cb
au2 <afsin@amida-tech.com>
2014-05-01 19:36:26 -0400

    vitals refactored to its own module

--- ccda/ccd.js
+++ ccda/ccd.js
@@ -11,6 +11,7 @@ var shared = require('./shared');
 
 var Patient = require('./demographics').Patient;
 var ResultsSection = require('./results').ResultsSection;
+var VitalSignsSection = require('./vitals').VitalSignsSection;
 
 var AgeObservation = Component.define("AgeObservation")
 .templateRoot("2.16.840.1.113883.10.20.22.4.31");
@@ -74,46 +75,6 @@ var ProblemsSection = Component.define("ProblemsSection")
   type: "problems"
 });
 
-
-var VitalSignObservation = Component.define("VitalSignObservation")
-.templateRoot("2.16.840.1.113883.10.20.22.4.27")
-.fields([
-  ["sourceIds","1..*", "h:id", shared.Identifier],
-  ["vitalName","1..1", "h:code", shared.ConceptDescriptor],
-  ["measuredAt", "1..1", "h:effectiveTime", shared.EffectiveTime],
-  ["physicalQuantity","1..1", "h:value[@xsi:type='PQ']", shared.PhysicalQuantity],
-  ["freeTextValue","0..1", "h:text", shared.TextWithReference],
-  ["interpretations", "0..*", "h:interpretationCode[@codeSystem='2.16.840.1.113883.5.83']", shared.SimplifiedCode]
-])
-.uriBuilder({
-  category: "entries",
-  type: "vitals"
-});
-
-
-var VitalSignsOrganizer = Component.define("VitalSignsOrganizer")
-.templateRoot("2.16.840.1.113883.10.20.22.4.26")
-.fields([
-  ["panelName","0..1", "h:code", shared.ConceptDescriptor],
-  ["sourceIds","1..*", "h:id", shared.Identifier],
-  ["vitals", "1..*", VitalSignObservation.xpath(), VitalSignObservation]
-])
-.uriBuilder({
-  category: "organizers",
-  type: "vitals"
-});
-
-
-var VitalSignsSection = Component.define("VitalSignsSection")
-.templateRoot("2.16.840.1.113883.10.20.22.2.4.1")
-.fields([
-  //["name","0..1", "h:code", shared.ConceptDescriptor],
-  ["panels","0..*", VitalSignsOrganizer.xpath(), VitalSignsOrganizer],
-])
-.uriBuilder({
-  category: "sections",
-  type: "vitals"
-});
 
 var ImmunizationRefusalReason = Component.define("ImmunizationRefusalReason")
 .templateRoot("2.16.840.1.113883.10.20.22.4.53");
