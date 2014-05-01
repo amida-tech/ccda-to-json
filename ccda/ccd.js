3c7b010f60dea3924d93b9b30fb16bae4caaf713
au2 <afsin@amida-tech.com>
2014-05-01 19:54:25 -0400

    Social History refactored to its own module

--- ccda/ccd.js
+++ ccda/ccd.js
@@ -14,6 +14,7 @@ var ResultsSection = require('./results').ResultsSection;
 var VitalSignsSection = require('./vitals').VitalSignsSection;
 var ProblemsSection = require('./problems').ProblemsSection;
 var ImmunizationsSection = require('./immunizations').ImmunizationsSection;
+var SocialHistorySection = require('./socialHistory').SocialHistorySection;
 
 var MedicationInformation = Component.define("MedicationInformation")
 .templateRoot("2.16.840.1.113883.10.20.22.4.23")
@@ -75,36 +76,6 @@ var MedActivityHx = MedicationActivity.define("MedActivityHx")
 .uriBuilder({
   category:"entries",
   type:"medicationsReported"
-});
-
-var SmokingStatusObservation = Component.define("SmokingStatusObservation")
-.templateRoot([
-  "2.16.840.1.113883.10.20.22.4.78", // the correct templateId
-  "2.16.840.1.113883.10.22.4.78" // incorrect id published in 1.1 DSTU
-])
-.fields([
-  ["sourceIds","1..*", "h:id", shared.Identifier],
-  ["smokingStatus","0..1", "h:value", 
-    shared.ConceptDescriptor.shall({
-      valueSetOid: "2.16.840.1.113883.11.20.9.38"
-    })],
-  // TODO: want a better name for this field -- but what does it mean?
-  // http://www.hl7.org/dstucomments/showdetail_comment.cfm?commentid=98
-  ["dateRange", "1..1", "h:effectiveTime", shared.EffectiveTime],
-])
-.uriBuilder({
-  category: "entries",
-  type: "smokingStatus"
-}) ;
-
-var SocialHistorySection = Component.define("SocialHistorSection")
-.templateRoot(["2.16.840.1.113883.10.20.22.2.17"])
-.fields([
-  ["smokingStatuses","0..*", SmokingStatusObservation.xpath(), SmokingStatusObservation]
-])
-.uriBuilder({
-  category: "sections",
-  type: "socialHistory"
 });
 
 var MedicationsSection = Component.define("MedicationsSection")
